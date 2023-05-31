38b39ffd04b4f59f29774d9584143845a827b0c1
goodben7 <bmukena85@gmail.com>
2023-05-31 15:32:59 +0200

    add form edit profil and form edit password

--- src/EntityListener/UserListener.php
+++ src/EntityListener/UserListener.php
@@ -16,11 +16,13 @@ class UserListener
     {
         $this->encodePassword($user);
     }
-
+    
+    /*
     public function preUpdate(User $user)
     {
         $this->encodePassword($user);
     }
+    */
 
     public function encodePassword(User $user)
     {
